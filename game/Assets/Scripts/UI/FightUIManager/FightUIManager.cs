4b597ce7014a59ff916daaaec53b5573bd05ddec
azurox <gauthier.valenza@gmail.com>
2018-08-23 14:02:15 +0200

    update scene

---
+++ game/Assets/Scripts/UI/FightUIManager/FightUIManager.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FightUIManager : MonoBehaviour {
+
+	public void SetReady()
+    {
+        Debug.Log("readdy");
+    }
+}
---
+++ game/Assets/Scripts/UI/GlobalUIManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GlobalUIManager : MonoBehaviour {
+
+    public FightUIManager FightUIManager;
+    public WorldUIManager WorldUIManager;
+
+    void Start () {
+        FightUIManager = GetComponentInChildren<FightUIManager>();
+        WorldUIManager = GetComponentInChildren<WorldUIManager>();
+    }
+	
+	public void SwitchToFightUI()
+    {
+        FightUIManager.gameObject.SetActive(true);
+        WorldUIManager.gameObject.SetActive(false);
+    }
+
+    public void SwitchToWorldtUI()
+    {
+        WorldUIManager.gameObject.SetActive(true);
+        FightUIManager.gameObject.SetActive(false);
+    }
+}
