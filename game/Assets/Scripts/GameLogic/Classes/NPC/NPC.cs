fd93265cc42c7e2d62f52bac345c09a8a8c932ee
azurox <gauthier.valenza@gmail.com>
2018-09-03 14:39:41 +0200

    NPC client side startup

---
+++ game/Assets/Scripts/GameLogic/Classes/NPC/NPC.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Npc {
+    public string[] scenariosId;
+    public Vector2 position;
+    public int orientation;
+    public string imageId;
+    public Dictionary<string, Scenario> scenarios = new Dictionary<string, Scenario>();
+}
---
+++ game/Assets/Scripts/GameLogic/Classes/NPC/NpcDTG.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NpcDTG : MonoBehaviour {
+    private Npc npc;
+    private Scenario scenario;
+    private Sprite sprite;
+
+    private void OnMouseDown()
+    {
+        Debug.Log("click on NPC");
+    }
+
+    public Scenario GetScenario()
+    {
+        return scenario;
+    }
+
+    public void SetScenario(Scenario scenario)
+    {
+        this.scenario = scenario;
+    }
+
+    public void SetNpc(Npc npc)
+    {
+        this.npc = npc;
+        foreach (var id in this.npc.scenariosId)
+        {
+            this.npc.scenarios[id] = ResourcesLoader.Instance.GetScenario(id);
+        }
+        sprite = ResourcesLoader.Instance.GetImage(npc.imageId);
+    }
+
+    public Npc GetNpc()
+    {
+        return npc;
+    }
+}
