3239fcb06c3475223afdf70292e1d694723088c2
azurox <gauthier.valenza@gmail.com>
2018-09-03 14:39:41 +0200

    NPC client side startup

---
+++ game/Assets/Scripts/GameLogic/Classes/NPC/NpcContainerDTG.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NpcContainerDTG : MonoBehaviour {
+
+    public NpcDTG NpcDTG;
+    private List<NpcDTG> npcs = new List<NpcDTG>();
+
+    public void LoadNPC(string id)
+    {
+        var npc = ResourcesLoader.Instance.GetNpc(id);
+        var go = Instantiate(NpcDTG, transform);
+        NpcDTG npcDtg = go.GetComponent<NpcDTG>();
+        npcDtg.SetNpc(npc);
+        npcs.Add(npcDtg);
+    }
+
+    public void Clear()
+    {
+        foreach (var npc in npcs)
+        {
+            Destroy(npc.gameObject);
+        }
+        npcs.Clear();
+    }
+	
+}
---
+++ game/Assets/Scripts/GameLogic/Classes/NPC/ScenarioResponse.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScenarioResponse : MonoBehaviour {
+    public string response;
+    public string scenarioId;
+}
