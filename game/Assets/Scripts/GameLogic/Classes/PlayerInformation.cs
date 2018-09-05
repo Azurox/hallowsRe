72d98df51539296e0c3c5e02a0a62729469b785f
azurox <gauthier.valenza@gmail.com>
2018-09-05 15:17:00 +0200

    Photo taker init

---
+++ game/Assets/Scripts/GameLogic/Classes/PlayerInformation.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerInformation : Singleton<PlayerInformation>
+{
+    protected PlayerInformation() { }
+
+    private Image playerImage;
+
+    private GameObject playerGameObject;
+    private GameObject fighterGameObject;
+
+    public Image GetPlayerImage()
+    {
+        return Utils.Instance.GetPhotoTaker().TakePicture(playerGameObject);
+    }
+
+}
